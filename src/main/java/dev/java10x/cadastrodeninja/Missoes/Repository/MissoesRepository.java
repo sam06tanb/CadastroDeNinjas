4bef29be442a795ef4be599021768440c3b2bbcf
sam06tanb <samirtajra53@gmail.com>
2025-01-06 17:01:14 -0300

    fix: erro no Jpa da classe MissoesRepository resolvido

--- src/main/java/dev/java10x/cadastrodeninja/Missoes/Repository/MissoesRepository.java
+++ src/main/java/dev/java10x/cadastrodeninja/Missoes/Repository/MissoesRepository.java
@@ -1,7 +1,8 @@
 package dev.java10x.cadastrodeninja.Missoes.Repository;
 
+import dev.java10x.cadastrodeninja.Missoes.Model.MissoesModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MissoesRepository extends JpaRepository {
+public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
 
 }
