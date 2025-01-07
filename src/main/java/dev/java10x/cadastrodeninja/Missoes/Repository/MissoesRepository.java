c3233a04c5802a8ed188ba671b30b6841ff9dc3b
sam06tanb <samirtajra53@gmail.com>
2025-01-07 17:39:42 -0300

    build: Conecta o banco de dados h2 a minha aplicacao spring

--- src/main/java/dev/java10x/cadastrodeninja/Missoes/Repository/MissoesRepository.java
+++ src/main/java/dev/java10x/cadastrodeninja/Missoes/Repository/MissoesRepository.java
@@ -4,5 +4,4 @@ import dev.java10x.cadastrodeninja.Missoes.Model.MissoesModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
-
 }
