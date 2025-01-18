83493d17d2381933e4ed80c06f2f91c7c19ef2cb
sam06tanb <samirtajra53@gmail.com>
2025-01-18 19:38:54 -0300

    chore: Inclui a versao do dto pela entidade e mapeia manualmente no NinjaMapper os caminhos entidade x dto

--- src/main/java/dev/java10x/cadastrodeninja/Ninjas/NinjaDTO.java
+++ src/main/java/dev/java10x/cadastrodeninja/Ninjas/NinjaDTO.java
@@ -1,4 +1,22 @@
 package dev.java10x.cadastrodeninja.Ninjas;
+import dev.java10x.cadastrodeninja.Missoes.Model.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
-}
+
+        private long id;
+        private String nome;
+        private String email;
+        private int idade;
+        private String rank;
+        private MissoesModel missoes;
+
+
+    }
+
+
--- src/main/java/dev/java10x/cadastrodeninja/Ninjas/NinjaMAPPER.java
+++ src/main/java/dev/java10x/cadastrodeninja/Ninjas/NinjaMAPPER.java
@@ -1,4 +1,34 @@
 package dev.java10x.cadastrodeninja.Ninjas;
 
+import dev.java10x.cadastrodeninja.Ninjas.Model.NinjaModel;
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMAPPER {
+
+    public NinjaModel map(NinjaDTO ninjaDTO) {
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setRank(ninjaDTO.getRank());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel) {
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setNome(ninjaModel.getNome());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setRank(ninjaModel.getRank());
+        ninjaDTO.setMissoes(ninjaModel.getMissoes());
+
+        return ninjaDTO;
+    }
+
+
 }
